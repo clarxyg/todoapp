5f470eb7478083dcea7ebdcc075ebefe737f4156
clarxyg <clarxyg@gmail.com>
2022-08-27 18:26:13 -0300

    Create custom hooks to render list and create task

--- src/Hooks/useCrud.jsx
+++ src/Hooks/useCrud.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 
 
 
-export function useCrud() {
-    
+export function useGet() {
+   const [list, setList] = useState([])
+
+   function handleList(task) {
+    if(task != '') {
+        setList([ ...list, task])
+    }
+   }
+
+    return [list, handleList]
+}
+
+export function useTask() {
+    const [task, setTask] = useState('')
+
+    return [task, setTask]
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,15 @@
 import { CheckSquare, Plus, Trash } from "phosphor-react";
+import { useGet, useTask } from "../Hooks/useCrud";
 import { SideBar } from "./Siderbar";
 import { Title } from "./Title";
 
 
 
 export function List() {
+    const [list, handleList] = useGet()
+    const [task, setTask] = useTask()
+
+
     return (
         <div className="App">
             <SideBar />
@@ -16,24 +21,28 @@ export function List() {
                     </h1>
                 </div>
                 <div style={{ display: 'flex', marginBottom: '100px' }}>
-                    <input placeholder="Digite aqui uma tarefa" />
+                    <input placeholder="Digite aqui uma tarefa" onChange={event => setTask(event.target.value)}/>
                     <button className="button_list">
-                        <Plus size={28} className="icon_plus" />
+                        <Plus size={28} className="icon_plus" onClick={() => handleList(task)}/>
                     </button>
                 </div>
-                <div className="container">
-                    <div className="container_card">
-                        <div className="card_list">
-                            <p style={{ paddingLeft: '15px' }}>
-                                Tarefa teste
-                            </p>
+                {list.map((text, index) => (
+                    <div className="gap" key={text}>
+                        <div className="container">
+                            <div className="container_card">
+                                <div className="card_list">
+                                    <p style={{ paddingLeft: '15px' }}>
+                                        {text}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="grid">
+                                <CheckSquare size={28} className="icon" />
+                                <Trash size={28} className="icon"/>
+                            </div>
                         </div>
                     </div>
-                    <div className="grid">
-                        <CheckSquare size={28} className="icon" />
-                        <Trash size={28} className="icon" />
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
 
